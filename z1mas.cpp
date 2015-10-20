f88fcffe819aa9223fc6a2cf8bad34922eb8f0a0
Valik.Vityaz <valik.vityaz@gmail.com>
2015-10-20 21:45:13 +0300

    Added tests for z1mas.cpp

--- z1mas.cpp
+++ z1mas.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+int testnum = 1;
 bool prog(char* stroka)
 {
 	int a = 0, b = 0, c = 0;
@@ -29,8 +29,20 @@ bool prog(char* stroka)
 	else return true;
 }
 
+void assert(char* s, bool realans)
+{
+	if (prog(s) == realans)
+		cout << testnum++ << ":You're right" << endl;
+	else
+		cout << testnum++ << ":Ooops!" << endl;
+}
 
 int main()
 {
+	assert("(((([)])))", false);
+	assert("()()[{}()]({[]})", true);
+	assert("", true);
+	assert("([{)]}", false);
+	assert("((([[[{{{}}}]]])))", true);
 	return 0;
 }
